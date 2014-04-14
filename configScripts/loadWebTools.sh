f7bb21d2d70cf1594e269b7f06d0bb6987d5ab0e
Michelle Shaw <mgshaw@silverrosesystems.ca>
2014-04-14 17:33:36 -0600

    add loadWebTools.sh
    Adding a script that takes a vanilla Raspbian Wheezy system and adds
    Apache2, MySQL, PHP and PHPMyAdmin to it.

---
+++ configScripts/loadWebTools.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#***********************************************************************
+#
+# loadWebTools.sh
+#
+# Purpose:
+#
+#  A short shell script to automate loading the Apache2 and PHP stack
+#  onto a Raspberry Pi running Raspbian.
+#
+# Notes:
+#	14-04-14  Tested with Raspbian Wheezy
+#
+# (C) 2014 SilverRose Systems Ltd.
+# License:  GNU Lesser GPL
+# 
+#  https://www.gnu.org/licenses/lgpl.html
+#
+apt-get -ym update
+apt-get -ym install apache2 apache2-doc apache2-utils
+apt-get -ym install libapache2-mod-php5 php5 php-pear php5-xcache
+apt-get -ym install php5-mysql
+apt-get -ym install mysql-server mysql-client
+apt-get -ym install pwgen
+apt-get -ym install phpmyadmin
+echo '#include phpmyadmin config' >> /etc/apache2/apache2.conf
+echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+/etc/init.d/apache2 restart
