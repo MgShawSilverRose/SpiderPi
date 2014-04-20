9b8d833387bcc60d26232ec102f6fb50e88128c3
Michelle Shaw <mgshaw@silverrosesystems.ca>
2014-04-20 11:19:20 -0600

    Create CMS Downlaod and Install Scripts
    Added getDrupal and getWordpress scripts to download the desired CMS,
    install it in /var/www and set up SQL databases for them.

--- cms/getDrupal.sh
+++ cms/getDrupal.sh
@@ -12,6 +12,14 @@
 #  https://www.gnu.org/licenses/lgpl.html
 #****************************************************************************
 #
+# If we aren't root, exit before we do any real damage
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+#
+mysqlPwd=$1
+#
 #
 # Step 1.  Download the latest Drupal
 # 
@@ -23,6 +31,7 @@ tar zxf ../drupal-*.gz
 #
 # Step 2.  Copy it to /var/www
 #
+/bin/rm -rf /var/www/*
 mv drupal-*/* /var/www
 #
 # Cleanup
@@ -30,4 +39,37 @@ mv drupal-*/* /var/www
 cd /var/tmp
 /bin/rm -f drupal-*.tar.gz
 /bin/rm -rf /var/tmp/drupal
-
+#
+# Step 2.  Create The MySQL Database
+#
+# Create Database on MySQL
+#
+dbnameSuffix=`pwgen -1`
+dbname="dru_$dbnameSuffix"
+sqlCmd="CREATE DATABASE $dbname"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd" mysql
+#
+# Create User on MySQL
+#
+dbuser="dru_$dbnameSuffix"
+dbpass=`pwgen -1`
+sqlCmd1="CREATE USER $dbuser@localhost IDENTIFIED BY '$dbpass'"
+sqlCmd2="GRANT ALL on $dbname.* to '$dbuser'@'localhost'"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd1"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd2"
+#
+# Record Configuration for user reference
+#
+echo "Drupal Database Name=$dbname" > /var/tmp/drupalConfig.txt
+echo "Drupal Database User=$dbuser" >> /var/tmp/drupalConfig.txt
+echo "Drupal Database User Password=$dbpass" >> /var/tmp/drupalConfig.txt
+#
+# Step 3.  Create Settings.php File
+#
+cp /var/www/sites/default/default.settings.php /var/www/sites/default/settings.php
+chmod a+w /var/sites/
+#
+# 
+cp /var/tmp/drupalConfig.txt ~pi
+chown pi ~pi/drupalConfig.txt
+cat ~pi/drupalConfig.txt
--- cms/getWordpress.sh
+++ cms/getWordpress.sh
@@ -6,12 +6,25 @@
 # A simple script to download and install wordpress files into the correct
 # location.
 #
+# Files Created:
+#    /var/tmp/wordpressConfig.txt
+#
+# Directories Modified:
+#   /var/www
+#
 # (C) 2014 SilverRose Systems Ltd.
 # License:  GNU Lesser GPL
 # 
 #  https://www.gnu.org/licenses/lgpl.html
 #******************************************************************************
+# If we aren't root, exit before we do any real damage
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 #
+# 
+mysqlPwd=$1
 #
 # Get the wordpress source 
 #
@@ -22,6 +35,7 @@ if [ ! -e latest.tar.gz ]; then
    exit 1
 fi
 tar zxf latest.tar.gz
+/bin/rm -rf /var/www/*
 mv wordpress/* /var/www
 #
 # Cleanup
@@ -32,6 +46,26 @@ mv wordpress/* /var/www
 #
 # Create Database on MySQL
 #
+dbnameSuffix=`pwgen -1`
+dbname="wp_$dbnameSuffix"
+sqlCmd="CREATE DATABASE $dbname"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd" mysql
+#
 # Create User on MySQL
 #
-
+dbuser="uwp_$dbnameSuffix"
+dbpass=`pwgen -1`
+sqlCmd1="CREATE USER $dbuser@localhost IDENTIFIED BY '$dbpass'" 
+sqlCmd2="GRANT ALL on $dbname.* to '$dbuser'@'localhost'"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd1"
+mysql --user=root --password=$mysqlPwd -e "$sqlCmd2"
+#
+# Record Configuration for user reference
+#
+echo "Wordpress Database Name=$dbname" > /var/tmp/wordpressConfig.txt
+echo "Wordpress Database User=$dbuser" >> /var/tmp/wordpressConfig.txt
+echo "Wordpress Database User Password=$dbpass" >> /var/tmp/wordpressConfig.txt
+cp /var/tmp/wordpressConfig.txt ~pi
+chown pi ~pi/wordpressConfig.txt
+/bin/rm -f /var/tmp/wordpressConfig.txt
+cat ~pi/wordpressConfig.txt
